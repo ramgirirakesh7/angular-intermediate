e3c65c85aadb3336bd9ba4a2444d2edf5ecbf829
ramgirirakesh7 <59351295+ramgirirakesh7@users.noreply.github.com>
2021-02-01 09:50:52 -0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { RouterModule } from '@angular/router';
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionnaireComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
